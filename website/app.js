a8dfc0bf99fb4c0aa3ad1a21cbad1546e23e3550
marcovigano <marco.vigano@ymail.com>
2020-06-14 15:26:03 +0100

    invalidated passwd - created new one with conf

--- website/app.js
+++ website/app.js
@@ -1,6 +1,7 @@
 /* Global Variables */
-const appKey = "6da71fbb322f5b1d543a8c95dd7c12d4";
+require('dotenv').config();
 
+const appKey = process.env.APPKEY_OPENWEATHER;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
