7ecea2ae0eb716f14664e1c3943ad7f69e4d14ed
marcovigano <marco.vigano@ymail.com>
2020-06-15 20:36:32 +0100

    a little bit more css

--- website/app.js
+++ website/app.js
@@ -1,8 +1,13 @@
 /* Global Variables */
 require('dotenv').config();
 
-const appKey = process.env.APPKEY_OPENWEATHER;
+// const appKey = process.env.APPKEY_OPENWEATHER;
+//TODO need to remove this key from here
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.addEventListener('click', () => {
+    console.log('hello');
+});
